27dcf2553e07fe51ffed0a07b5922657698fbd8d
Giuliana <giuliana@myelios.com>
2021-01-28 17:22:00 -0700

    Adding SelectEmail

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,11 +19,11 @@ class _MyAppState extends State<MyApp> {
   Contact _contact;
 
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
+  Widget build(BuildContext context) =>
+     new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Plugin example app'),
+          title:  const Text('Plugin example app'),
         ),
         body: new Center(
           child: new Column(
@@ -31,9 +31,9 @@ class _MyAppState extends State<MyApp> {
             children: <Widget>[
               new MaterialButton(
                 color: Colors.blue,
-                child: new Text("CLICK ME"),
+                child:  const Text("CLICK ME"),
                 onPressed: () async {
-                  Contact contact = await _contactPicker.selectEmail();
+                  final Contact contact = await _contactPicker.selectEmail();
                   setState(() {
                     _contact = contact;
                   });
@@ -47,5 +47,5 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
-  }
+
 }
--- ios/Classes/ContactPickerPlugin.m
+++ ios/Classes/ContactPickerPlugin.m
@@ -21,37 +21,81 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-  if ([@"selectContact" isEqualToString:call.method]) {
-    if (_result) {
-      _result([FlutterError errorWithCode:@"multiple_requests"
+    NSArray *myMethods = [NSArray arrayWithObjects: @"selectPhone", @"selectContact", @"selectEmail", nil];
+    // Check if the method call exists.
+    if ([myMethods containsObject:call.method])
+    {
+        if (_result)
+        {
+            //Making custom error Flutter reply to method called
+            _result([FlutterError errorWithCode:@"multiple_requests"
                                   message:@"Cancelled by a second request."
                                   details:nil]);
-      _result = nil;
-    }
-    _result = result;
+            _result = nil;
+        }
+        
+        _result = result;
 
-    CNContactPickerViewController *contactPicker = [[CNContactPickerViewController alloc] init];
-    contactPicker.delegate = self;
-    contactPicker.displayedPropertyKeys = @[ CNContactPhoneNumbersKey ];
-
-    UIViewController *viewController =
-        [UIApplication sharedApplication].delegate.window.rootViewController;
-    [viewController presentViewController:contactPicker animated:YES completion:nil];
-  } else {
-    result(FlutterMethodNotImplemented);
+        CNContactPickerViewController *contactPicker = [[CNContactPickerViewController alloc] init];
+        contactPicker.delegate = self;
+      
+        //Check what method was selected.
+        if([@"selectEmail" isEqualToString:call.method])
+        {
+            contactPicker.displayedPropertyKeys = @[ CNContactEmailAddressesKey ];
+        } else
+        {
+            contactPicker.displayedPropertyKeys = @[ CNContactPhoneNumbersKey ];
+        }
+      
+        UIViewController *viewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+        [viewController presentViewController:contactPicker animated:YES completion:nil];
   }
+    else
+    {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 - (void)contactPicker:(CNContactPickerViewController *)picker
     didSelectContactProperty:(CNContactProperty *)contactProperty {
+    //Getting Contact Name
   NSString *fullName = [CNContactFormatter stringFromContact:contactProperty.contact
                                                        style:CNContactFormatterStyleFullName];
-  NSDictionary *phoneNumber = [NSDictionary
-      dictionaryWithObjectsAndKeys:[contactProperty.value stringValue], @"number",
-                                   [CNLabeledValue localizedStringForLabel:contactProperty.label],
-                                   @"label", nil];
-  _result([NSDictionary
-      dictionaryWithObjectsAndKeys:fullName, @"fullName", phoneNumber, @"phoneNumber", nil]);
+    
+    //Defining variables and setting default values
+    NSString *contactInfoType = @"number";
+    NSString *contactDictionaryType = @"phoneNumber";
+    NSString *contactInfo = nil;
+    NSString *contactLabel = nil;
+    NSDictionary *contactDictionary = nil;
+    
+    //Getting contact Property type. email or phone.
+    NSString *contactType = contactProperty.key;
+    
+     //Check if contact is an email or phone
+  if([contactType rangeOfString:@"email"].location != NSNotFound){
+      // Changing default values
+     contactInfoType = @"address";
+     contactDictionaryType = @"emailAddress";
+      
+     //Creating new object with a contact property and label;
+     contactInfo = contactProperty.contact.emailAddresses.firstObject.value;
+     contactLabel = contactProperty.contact.emailAddresses.firstObject.label;
+ }
+    //Creating new object with a contact property and label;
+  else{
+      contactInfo = contactProperty.contact.phoneNumbers.firstObject.value.stringValue;
+      contactLabel = contactProperty.label;
+      }
+    
+    //Creating a String Dictionary with [emailAddress : address, emailLabel : label];
+   contactDictionary = [NSDictionary dictionaryWithObjectsAndKeys: contactInfo, contactInfoType,  [CNLabeledValue localizedStringForLabel:contactLabel], @"label", nil];
+
+    //Saving fullName and emailAddress Dictionary into result
+    _result([NSDictionary
+        dictionaryWithObjectsAndKeys:fullName, @"fullName", contactDictionary, contactDictionaryType, nil]);
+ 
   _result = nil;
 }
 
